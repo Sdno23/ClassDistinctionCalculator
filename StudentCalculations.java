2f3dba8459e18053ce38f43650d7999873d7287f
Sdno23 <103979026+Sdno23@users.noreply.github.com>
2023-11-06 02:21:33 -1000

    Create StudentCalculations.java

---
+++ StudentCalculations.java
@@ -0,0 +1,24 @@
+/*
+ * @author XXXXX08
+ *
+ * @version 1.0 09/20/2020
+ *
+ * Title: Polymorphism Student, K12Student, CollegeStudent
+ *
+ * Semester: Fall 2020
+ *
+ * Lecturer's Name: C. Charters.
+ */
+
+package classdistinctioncalculator;
+
+/**
+ *
+ * @author XXXXX08
+ */
+
+public interface StudentCalculations {
+    
+    
+    public int calculateYearsLeft();
+}
